6661706a1359bbfc2339bb905fe233e7cdc0688b
Patrick Haberern <patrick@Patricks-MacBook-Pro.local>
2019-04-16 17:35:23 -0400

    Program works. Will tidy up and then ready for final commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,27 +1,28 @@
 // Array of strings based on topics
-topics = [
+games = [
     "Halo", "Fortnite", "PUBG", "Apex Legends", "Call of Duty"
 ]
 
 // Loop through the array to create buttons based on each
-var renderButtons = function (topics) {
+var renderButtons = function (games) {
     $("#buttons").empty();
-    for (let i = 0; i < topics.length; i++) {
-        var button = $("<button>").text(topics[i]);
-        button.attr("data-name", topics[i]).addClass("btn btn-primary");
+    for (let i = 0; i < games.length; i++) {
+        var button = $("<button>").text(games[i]);
+        button.attr("data-name", games[i]).addClass("btn btn-primary");
         $("#buttons").append(button);
     }
 }
-renderButtons(topics);
+renderButtons(games);
 
 // ON click event for any of the buttons that have been rendered: 
-$(".btn").on("click", function () {
+$("#buttons").on("click",".btn", function () {
+    console.log("you clicked!");
     //Build the queryURL based on the button clicked
     var searchFor = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchFor + "&limit=10" + "&api_key=F0y8OeTPpYSZkVLz2fLvNXdxqtpfpPSp";
 
 
-//Ajax to call the Giphy API with the queryURL
+    //Ajax to call the Giphy API with the queryURL
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -53,9 +54,10 @@ $("#gifs").on("click", ".gif", function () {
     }
 })
 
-
-$("#add-game").on("click", function(event){
+// On click for submit button to ignore the auto response and call renderButtons()
+$("#add-game").on("click", function (event) {
     event.preventDefault();
     var gameInput = $("#game-input").val().trim();
+    games.push(gameInput);
+    renderButtons(games);
 })
-// On click for submit button to ignore the auto response and call renderButtons()
