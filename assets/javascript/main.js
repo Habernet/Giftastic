05f5dcc3fb5519128d1167438e80666d162f1217
Patrick Haberern <patrick@Patricks-MacBook-Pro.local>
2019-04-16 18:08:46 -0400

    Added a random index for pulling from a larger source of gifs. Pool is 100 gifs.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,6 @@
 // Array of strings based on topics
 games = [
-    "Halo", "Fortnite", "PUBG", "Apex Legends", "Call of Duty"
+    "Halo: Combat Evolved", "Fortnite", "PUBG", "Apex Legends", "Call of Duty"
 ]
 
 // Loop through the array to create buttons based on each
@@ -14,32 +14,31 @@ var renderButtons = function (games) {
 }
 renderButtons(games);
 
-// ON click event for any of the buttons that have been rendered: 
+// On click event for any of the buttons that have been rendered: 
 $("#buttons").on("click",".btn", function () {
-    console.log("you clicked!");
     //Build the queryURL based on the button clicked
     var searchFor = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchFor + "&limit=10" + "&api_key=F0y8OeTPpYSZkVLz2fLvNXdxqtpfpPSp";
-
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchFor + "&limit=100" + "&api_key=F0y8OeTPpYSZkVLz2fLvNXdxqtpfpPSp";
 
     //Ajax to call the Giphy API with the queryURL
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        
         for (let i = 0; i < 10; i++) {
+            var randomIndex = Math.floor(Math.random() * 100);
             var gifDiv = $("<div>");
-            var gif = $("<img>").attr("src", response.data[i].images.fixed_height_still.url).addClass("gif");
+            var gif = $("<img>").attr("src", response.data[randomIndex].images.fixed_height_still.url).addClass("gif");
             gif.attr("data-state", "still");
-            gif.attr("data-still", response.data[i].images.fixed_height_still.url);
-            gif.attr("data-animate", response.data[i].images.fixed_height.url);
-            var rating = $("<p>").text("Rating: " + response.data[i].rating);
+            gif.attr("data-still", response.data[randomIndex].images.fixed_height_still.url);
+            gif.attr("data-animate", response.data[randomIndex].images.fixed_height.url);
+            var rating = $("<p>").text("Rating: " + response.data[randomIndex].rating);
             gifDiv.append(gif, rating);
             $("#gifs").prepend(gifDiv);
         }
     })
 })
-
 
 // On click event to change the state of the gif to active, then set the state so if it's clicked again/tested again it will change its state to the other state.
 $("#gifs").on("click", ".gif", function () {
